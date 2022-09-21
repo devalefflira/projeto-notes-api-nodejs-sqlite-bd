8633c2382aef1b6d381ef98e1d08484db3a1161b
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-09-21 14:24:37 -0300

    Criação do projeto, inicialização do servidor, configuração da rota, observação do servidor com nodemon

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+    ID message: ${id}.
+    To the user: ${user}.
+  `);
+});
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Page: ${page}. Show: ${limit}`);
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
