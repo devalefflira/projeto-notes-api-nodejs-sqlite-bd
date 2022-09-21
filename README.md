5111e9818f21345fd713871cb8d7fd95e2a2d03f
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2022-09-21 15:34:33 -0300

    Atualização dos parâmetros, utilização do método POST, Insomnia e estruturação do projeto

---
+++ README.md
@@ -0,0 +1,31 @@
+# Projeto API Notes
+
+## Desenvolvido no,
+
+### Back-end, com:
+
+- Nodejs
+- Sqlite
+
+## O quê eu aprendi?
+
+### com Nodejs:
+
+- Funcionamento do Node
+- Iniciar um projeto Nodejs
+- Adicionar o Express
+- node_modules
+- Utilizar o Express
+- Executar a aplicação
+- Rotas e Métodos HTTP
+- Método GET
+- Route Params
+- Query Params
+- Nodemon
+- Insomnia
+- Método POST
+- Body Params
+- Organizar a estrutura do projeto
+- Controllers
+
+Última atualização do código em: 21/09/2022, as 15:27.
--- src/server.js
+++ src/server.js
@@ -1,19 +1,12 @@
 const express = require('express');
+
 const app = express();
+app.use(express.json());
 
-app.get('/message/:id/:user', (request, response) => {
-  const { id, user } = request.params;
+app.post('/users', (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`
-    ID message: ${id}.
-    To the user: ${user}.
-  `);
-});
-
-app.get('/users', (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Page: ${page}. Show: ${limit}`);
+  response.send(`User: ${name} - E-mail: ${email} - Password: ${password}`);
 });
 
 const PORT = 3333;
